2effcf9b43af4825c17b355cc07a4659b5e5ba0f
Sajeev Ramasamy <thorion3006@gmail.com>
2017-07-23 21:07:55 +0000

    completed

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -1,4 +1,5 @@
 require_relative 'node'
+require 'benchmark'
 
 class LinkedList
   attr_accessor :head
@@ -6,25 +7,70 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    @head ||= node
+    @head.next = node if @head.next.nil? && @head != node
+    @tail.next = node if @tail
+    @tail = node
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    if @head == @tail
+      @head = nil
+      @tail = nil
+    else
+      temp1 = @head
+      temp2 = temp1.next
+      while temp2.next
+        temp1 = temp2
+        temp2 = temp2.next
+      end
+      @tail = temp1
+      @tail.next = nil
+      @head.next = nil if @head == temp1
+    end
   end
 
   # This method prints out a representation of the list.
   def print
+    temp = @head
+    puts temp.data
+    while temp.next
+      puts temp.next.data
+      temp = temp.next
+    end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    temp = @head.next
+    if @head == node
+      @head = temp
+    elsif @tail == node
+      remove_tail
+    else
+      prev_temp = @head
+      while temp.next
+        if temp == node
+          prev_temp.next = temp.next
+          break
+        end
+        prev_temp = temp
+        temp = temp.next
+      end
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    node.next = @head
+    @head = node
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    front = @head
+    @head = front.next
+    front
   end
 end
