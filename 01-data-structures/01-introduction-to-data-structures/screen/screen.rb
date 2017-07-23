c2c25dc687906554fcb43e8efa32bc63e3368d56
Sajeev Ramasamy <thorion3006@gmail.com>
2017-07-23 15:53:05 +0000

    completed

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,23 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    @width = width
+    @height = height
+    @matrix = []
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    matrix[x, y] = pixel
   end
 
   def at(x, y)
+    matrix[x, y].first if inbounds(x, y)
   end
 
   private
 
   def inbounds(x, y)
+    x > 0 && y > 0 ? true : false
   end
-
 end
