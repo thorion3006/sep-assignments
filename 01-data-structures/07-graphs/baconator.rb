3179c070ef0e92ad0c648cfd6cef7c2cdbdfee32
Sajeev Ramasamy <thorion3006@gmail.com>
2017-08-14 12:44:39 +0400

    complete

--- 01-data-structures/07-graphs/baconator.rb
+++ 01-data-structures/07-graphs/baconator.rb
@@ -3,9 +3,47 @@ require_relative 'node'
 class Baconator
   def initialize(node)
     @root = node
+    @root.previous = nil
+    @root.weight = 0
   end
 
   def find_kevin_bacon
-    
+    queue = []
+    root = bfs
+    while root.previous
+      queue.push(root)
+      root = root.previous
+    end
+    movies = []
+    while queue.any?
+      actor = queue.pop
+      movies.push(actor.movie)
+    end
+    movies
+  end
+
+  def bfs
+    queue = []
+    @root.film_actor_hash.each do |movie, actors|
+      actors.each do |actor|
+        actor.previous = @root
+        actor.weight = @root.weight + 1
+        actor.movie = movie
+        return actor if actor.name == 'Kevin Bacon'
+        queue.push(actor)
+      end
+    end
+    while queue.any?
+      root = queue.shift
+      root.film_actor_hash.each do |movie, actors|
+        actors.each do |actor|
+          actor.movie = movie
+          actor.previous = root
+          actor.weight = root.weight + 1
+          return actor if actor.name == 'Kevin Bacon'
+          queue.push(actor) if actor.weight <= 5
+        end
+      end
+    end
   end
 end
--- 01-data-structures/07-graphs/node.rb
+++ 01-data-structures/07-graphs/node.rb
@@ -2,6 +2,9 @@
 class Node
   attr_accessor :name
   attr_accessor :film_actor_hash
+  attr_accessor :previous
+  attr_accessor :movie
+  attr_accessor :weight
 
   def initialize(name)
     @name = name
